bbe1d054637ffd6af39843c6293eb781620e3286
Darren J. McLeod <tempuskg@gmail.com>
2023-08-28 12:24:11 -0600

    refactored to use MS Test and behaviour test naming

--- csharpcore-Verify.xunit/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore-Verify.xunit/GildedRoseTests/GildedRoseTest.cs
@@ -1,13 +1,16 @@
-﻿using Xunit;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using GildedRoseKata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assert = Xunit.Assert;
 
 namespace GildedRoseTests
 {
-    public class GildedRoseTest
+    [TestClass]
+    public class WhenRunningGildedRose
     {
-        [Fact]
-        public void ItemsListOrderNotChanged()
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void It_should_not_change_items_list_order()
         {
             IList<Item> items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             var app = new GildedRose(items);
