24f4ad385be1a6521b0c375c595b25b5f7288c25
Darren J. McLeod <tempuskg@gmail.com>
2023-08-28 12:13:44 -0600

    refactored tests

--- csharpcore-Verify.xunit/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore-Verify.xunit/GildedRoseTests/GildedRoseTest.cs
@@ -9,10 +9,10 @@ namespace GildedRoseTests
         [Fact]
         public void ItemsListOrderNotChanged()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
+            IList<Item> items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            var app = new GildedRose(items);
             app.UpdateQuality();
-            Assert.Equal("foo", Items[0].Name);
+            Assert.Equal("foo", items[0].Name);
         }
     }
 }
