d9b0928821c364ce21c5397aad40f291c657c94b
Darren J. McLeod <tempuskg@gmail.com>
2023-08-29 16:09:10 -0600

    refactored Sulfuras DegradeQuality

--- csharpcore-Verify.xunit/GildedRose/Good.cs
+++ csharpcore-Verify.xunit/GildedRose/Good.cs
@@ -4,11 +4,11 @@ namespace GildedRoseKata
 {
     public abstract class Good
     {
-        private readonly Item   _item;
-        public const     string AgedBrie             = "Aged Brie";
-        public const     string BackStageTAFKAL80ETC = "Backstage passes to a TAFKAL80ETC concert";
-        public const     string Sulfuras             = "Sulfuras, Hand of Ragnaros";
-        public const     string Conjured             = "Conjured Mana Cake";
+        private readonly Item _item;
+        public const string AgedBrie = "Aged Brie";
+        public const string BackStageTAFKAL80ETC = "Backstage passes to a TAFKAL80ETC concert";
+        public const string Sulfuras = "Sulfuras, Hand of Ragnaros";
+        public const string Conjured = "Conjured Mana Cake";
 
         protected Good(Item item)
         {
@@ -19,53 +19,41 @@ namespace GildedRoseKata
         {
             switch (item.Name)
             {
-                case AgedBrie:                                 return new AgedBrie(item);
+                case AgedBrie: return new AgedBrie(item);
                 case BackStageTAFKAL80ETC: return new BackStageTAFKAL80ETC(item);
                 case Sulfuras: return new Sulfuras(item);
-                default:                                          return new GoodNullable(item);
+                default: return new GoodNullable(item);
             }
         }
 
-        public virtual string Name    => _item.Name;
-        public         int    Quality
+        public virtual string Name => _item.Name;
+        public int Quality
         {
             get => _item.Quality;
             set => _item.Quality = value;
         }
 
-        public int SellIn 
-        { 
+        public int SellIn
+        {
             get => _item.SellIn;
             set => _item.SellIn = value;
         }
 
         public virtual void DegradeQuality()
         {
-                if (Quality > 0)
-                {
-                    if (Name != Good.Sulfuras)
-                    {
-                        Quality = Quality - 1;
-                    }
-                }
+            if (Quality > 0)
+            {
+                Quality--;
+            }
 
-                if (Name != Good.Sulfuras)
-                {
-                    SellIn = SellIn - 1;
-                }
+            SellIn--;
 
-                if (SellIn < 0)
-                {
-                
-                    if (Quality > 0)
-                    {
-                        if (Name != Good.Sulfuras)
-                        {
-                            Quality = Quality - 1;
-                        }
-                    }
-                
-                }
+            if (SellIn >= 0) return;
+            
+            if (Quality > 0)
+            {
+                Quality--;
+            }
         }
     }
 }
--- csharpcore-Verify.xunit/GildedRose/Goods/Sulfuras.cs
+++ csharpcore-Verify.xunit/GildedRose/Goods/Sulfuras.cs
@@ -5,4 +5,9 @@ public class Sulfuras : Good
     public Sulfuras(Item item) : base(item)
     {
     }
+
+    public override void DegradeQuality()
+    {
+        //Sulfuras doesn't degrade
+    }
 }
