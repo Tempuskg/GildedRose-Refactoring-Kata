67e0d39575d4a2495b7593ea47635efa179d416b
Darren J. McLeod <tempuskg@gmail.com>
2023-08-28 18:10:27 -0600

    added AgedBrie sellin unit test

--- csharpcore-Verify.xunit/GildedRoseTests/Goods/WhenUsingAnAgedBrieItem.cs
+++ csharpcore-Verify.xunit/GildedRoseTests/Goods/WhenUsingAnAgedBrieItem.cs
@@ -23,5 +23,23 @@ namespace GildedRoseTests.Goods
             var degradedAmount = beginningQuality - anAgedBrieItem.Quality;
             degradedAmount.Should().Be(-1);
         }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void AgedBrie_items_with_sellin_less_than_or_equal_to_0_should_increase_quality_by_2()
+        {
+            var anAgedBrieItem = TestSetup.AnAgedBrieItem();
+            anAgedBrieItem.Quality = 1;
+            anAgedBrieItem.SellIn  = 0;
+            IList<Item> items            = new List<Item> { anAgedBrieItem };
+            var         app              = new GildedRose(items);
+            var         beginningQuality = anAgedBrieItem.Quality;
+
+            app.UpdateQuality();
+
+            var degradedAmount = beginningQuality - anAgedBrieItem.Quality;
+            degradedAmount.Should().Be(-2);
+        }
+
     }
 }
