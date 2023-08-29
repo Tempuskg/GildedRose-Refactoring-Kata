cf59f7e05dc7bfeaff1df96a243965b901753126
Darren J. McLeod <tempuskg@gmail.com>
2023-08-29 17:15:30 -0600

    refactored Program class

--- csharpcore-Verify.xunit/GildedRose/Program.cs
+++ csharpcore-Verify.xunit/GildedRose/Program.cs
@@ -9,44 +9,44 @@ namespace GildedRoseKata
         {
             Console.WriteLine("OMGHAI!");
 
-            IList<Item> Items = new List<Item>{
-                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
-                new Item
+            IList<Item> items = new List<Item>{
+                new() {Name = "+5 Dexterity Vest", SellIn          = 10, Quality = 20},
+                new() {Name = "Aged Brie", SellIn                  = 2, Quality  = 0},
+                new() {Name = "Elixir of the Mongoose", SellIn     = 5, Quality  = 7},
+                new() {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality  = 80},
+                new() {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+                new()
                 {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 15,
                     Quality = 20
                 },
-                new Item
+                new()
                 {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 10,
                     Quality = 49
                 },
-                new Item
+                new()
                 {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 5,
                     Quality = 49
                 },
-				// this conjured item does not work properly yet
-				new Item {Name = GoodBase.Conjured, SellIn = 3, Quality = 6}
+				
+				new() {Name = GoodBase.Conjured, SellIn = 3, Quality = 6}
             };
 
-            var app = new GildedRose(Items);
+            var app = new GildedRose(items);
 
 
             for (var i = 0; i < 31; i++)
             {
                 Console.WriteLine("-------- day " + i + " --------");
                 Console.WriteLine("name, sellIn, quality");
-                for (var j = 0; j < Items.Count; j++)
+                foreach (var t in items)
                 {
-                    System.Console.WriteLine(Items[j].Name + ", " + Items[j].SellIn + ", " + Items[j].Quality);
+                    Console.WriteLine(t.Name + ", " + t.SellIn + ", " + t.Quality);
                 }
                 Console.WriteLine("");
                 app.UpdateQuality();
